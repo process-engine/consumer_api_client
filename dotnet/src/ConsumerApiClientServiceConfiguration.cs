902cd50d6a24ab677e426a27150ac5f14380a8a4
5ebastianMeier <sebastian.meier@5minds.de>
2019-01-17 18:38:13 +0100

    add first basic structure

---
+++ dotnet/src/ConsumerApiClientServiceConfiguration.cs
@@ -0,0 +1,10 @@
+namespace ProcessEngine.ConsumerAPI.Client
+{
+
+    public class ConsumerApiClientServiceConfiguration {
+
+      public string BaseUrl { get; set; }
+
+      public string SocketUrl { get; set; }
+    }
+}
