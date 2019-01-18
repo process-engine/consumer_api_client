955d63cee9efa739dba3315c415a540256839df5
5ebastianMeier <sebastian.meier@5minds.de>
2019-01-18 11:18:59 +0100

    update consumer api client service implementation

--- dotnet/src/ConsumerApiClientService.cs
+++ dotnet/src/ConsumerApiClientService.cs
@@ -1,133 +1,115 @@
 ﻿namespace ProcessEngine.ConsumerAPI.Client
 {
-    using System;
-    using ProcessEngine.ConsumerAPI.Contracts;
-    using Quobject.SocketIoClientDotNet.Client;
+  using System;
+  using ProcessEngine.ConsumerAPI.Contracts;
+  using EssentialProjects.IAM.Contracts;
+  using Quobject.SocketIoClientDotNet.Client;
+  using System.Net.Http;
+  using System.Net.Http.Headers;
+  using System.Threading.Tasks;
+  using Newtonsoft.Json;
+  using System.Collections.Generic;
 
-    public class ConsumerApiClientService : IConsumerAPI
+  public class ConsumerApiClientService : IConsumerAPI
+  {
+
+    public ConsumerApiClientServiceConfiguration Configuration { get; set; }
+
+    public async Task<TProcessStartResponsePayload> StartProcessInstanceAsync<TProcessStartRequestPayload, TProcessStartResponsePayload>(
+          IIdentity identity,
+          string processModelId,
+          string startEventKey,
+          TProcessStartRequestPayload payload,
+          StartCallbackType callbackType = StartCallbackType.CallbackOnProcessInstanceCreated,
+          string endEventKey = "")
+          where TProcessStartResponsePayload : new()
     {
 
-      private delegate void ProcessEndedDelegate();
+      var noEndEventIdProvided = callbackType == StartCallbackType.CallbackOnEndEventReached
+        && String.IsNullOrEmpty(endEventKey);
 
-      private ProcessEndedDelegate ProcessEnded = new ProcessEndedDelegate();
-      private Socket Socket { get; set; }
-
-      public ConsumerApiClientServiceConfiguration Configuration { get; set; }
-
-      public void initializeSocket() {
-        
-            var socketUrl = this.Configuration.SocketUrl;
-            this.Socket = IO.Socket(socketUrl);
-
-            // ACHTUNG: Dieser Client ist bereits deprecated aber der Ersatz von
-            // IBM kann nur Rooms verwenden (hilft uns nicht)
-            //
-            // Siehe:
-            // https://github.com/Quobject/EngineIoClientDotNet/issues/69
-            // https://github.com/IBM/socket-io
-            //
-            // Laut Issues ist von diesem Paket 1.0.2 die letzte funktionierende
-            // Version. Das konnte ich noch nicht testen (SM).
-
-            this.Socket.On(socket.EVENT_OPEN, () => {
-
-              this.Socket.On("process_ended", () => {
-                this.ProcessEnded();
-              });
-            });
-
-            this.Socket.Open();
+      if (noEndEventIdProvided)
+      {
+        throw new ArgumentNullException(nameof(endEventKey));
       }
 
-      public void Dispose() {
+      var url = $"/process_models/{processModelId}/start_events/{startEventKey}/start";
 
-            this.Socket.Close();
+      var attachEndEventId = callbackType == StartCallbackType.CallbackOnEndEventReached;
+
+      if (attachEndEventId)
+      {
+        url = $"{url}&end_event_id={endEventKey}";
       }
 
-      private HttpClient _createHttpClient(IIdentity identity) {
-            var client = new HttpClient(new HttpClientHandler()
-            {
-                UseDefaultCredentials = true
-            });
-            client.BaseAddress = new Uri(this.Configuration.BaseUrl);
+      var jsonResult = "";
 
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            
-            var token = identity.token;
+      using(var client = _createHttpClient(identity))
+      {
+        var result = await client.PostAsJsonAsync(url, payload);
 
-            if (token == null) {
-              throw new UnauthorizedAccessException();
-            }
-
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", identity.token);
-            
-            return client;
+        if (result.IsSuccessStatusCode)
+        {
+          jsonResult = await result.Content.ReadAsStringAsync();
+          var parsedResult = JsonConvert.DeserializeObject<TProcessStartResponsePayload>(jsonResult);
+          return parsedResult;
+        }
       }
 
-      public Task<TProcessStartResponsePayload> StartProcessInstance<TProcessStartRequestPayload, TProcessStartResponsePayload>(
-            string identity,
-            string processModelId,
-            string startEventKey,
-            TProcessStartRequestPayload payload,
-            StartCallbackType callbackType = StartCallbackType.CallbackOnProcessInstanceCreated,
-            string endEventKey = "",
-            Delegate processEndedCallback = null)
-            where TProcessStartResponsePayload : new() {
-
-            var url = $"/process_models/{processModelId}/start_events/{startEventKey}/start";
-
-            var attachEndEventId = startCallbackType == StartCallbackType.CallbackOnEndEventReached;
-            
-            if (attachEndEventId) {
-              url = $"{url}&end_event_id={endEventKey}";
-            }
-
-            var jsonResult = "";
-
-            var client = _createHttpClient(identity);
-
-            var callbackProvided = processEndedCallback != null;
-
-            if (callbackProvided) {
-              // Hier müsste noch eine ID-Prüfung stattfinden
-              // scheinbar fehlt die aber auch bei der TS-Variante
-              this.ProcessEnded += processEndedCallback;
-            }
-
-            var result = await client.PostAsJsonAsync(url, payload);
-
-            if (result.IsSuccessStatusCode) {
-                jsonResult = await result.Content.ReadAsStringAsync();
-                var parsedResult = JsonConvert.DeserializeObject<TProcessStartResponsePayload>(jsonResult);
-                return parsedResult;
-            }
-
-            if (callbackProvided) {
-              this.ProcessEnded -= processEndedCallback;
-            }
-
-            return null;
-      }
-
-      Task<IEnumerable<CorrelationResult<TPayload>>> GetProcessResultForCorrelation<TPayload>(string identity, string correlationId, string processModelId)
-            where TPayload : new() {
-            
-            var url = $"/correlations/{correlationId}/process_models/${processModelId}/results";
-
-            var jsonResult = "";
-
-            var client = _createHttpClient(identity);
-
-            var result = await client.GetAsync(url);
-
-            if (result.IsSuccessStatusCode) {
-                jsonResult = await result.Content.ReadAsStringAsync();
-                var parsedResult = JsonConvert.DeserializeObject<IEnumerable<CorrelationResult<TPayload>>>(jsonResult);
-                return parsedResult;
-            }
-
-            return null;
-      }
+      return default(TProcessStartResponsePayload);
     }
+
+    public async Task<IEnumerable<CorrelationResult<TPayload>>> GetProcessResultForCorrelation<TPayload>(
+      IIdentity identity,
+      string correlationId,
+      string processModelId)
+          where TPayload : new()
+    {
+      var url = $"/correlations/{correlationId}/process_models/${processModelId}/results";
+
+      var jsonResult = "";
+
+      using(var client = _createHttpClient(identity))
+      {
+        var result = await client.GetAsync(url);
+
+        if (result.IsSuccessStatusCode)
+        {
+          jsonResult = await result.Content.ReadAsStringAsync();
+          var parsedResult = JsonConvert.DeserializeObject<IEnumerable<CorrelationResult<TPayload>>>(jsonResult);
+          return parsedResult;
+        }
+      }
+
+      return null;
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    private HttpClient _createHttpClient(IIdentity identity)
+    {
+      var client = new HttpClient(new HttpClientHandler()
+      {
+        UseDefaultCredentials = true
+      });
+      client.BaseAddress = new Uri(this.Configuration.BaseUrl);
+
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+      var token = identity.token;
+
+      if (token == null)
+      {
+        throw new UnauthorizedAccessException();
+      }
+
+      client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", identity.token);
+
+      return client;
+    }
+  }
 }
