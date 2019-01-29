0616fed4b6d3231345f3a03aa4263859d3593f01
Christian W <christian.werner@5minds.de>
2019-01-29 18:03:06 +0100

    :recycle: requested changes in pr
    Co-Authored-By: roblen45 <36449148+roblen45@users.noreply.github.com>

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-import { HttpClient } from '@essential-projects/http';
-import { ExternalAccessor, ConsumerApiClientService } from '@process-engine/consumer_api_client';
+import {HttpClient} from '@essential-projects/http';
+import {ConsumerApiClientService, ExternalAccessor} from '@process-engine/consumer_api_client';
 
 export {
   HttpClient,
