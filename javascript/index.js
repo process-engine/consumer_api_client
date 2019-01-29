2dbed89466f2a486db8d8faa994b4061e0f397b5
Robin <robin.lenz@5minds.de>
2019-01-29 14:59:09 +0100

    :sparkles: added javascript client

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+import { HttpClient } from '@essential-projects/http';
+import { ExternalAccessor, ConsumerApiClientService } from '@process-engine/consumer_api_client';
+
+export {
+  HttpClient,
+  ExternalAccessor,
+  ConsumerApiClientService
+}
