e8df10d1d8e36af28f81f4b16d7761fbb4dc6859
Sebastian Meier <sebastian.meier@5minds.de>
2019-03-06 16:06:35 +0100

    implement tests for manual task consumer api

---
+++ dotnet/tests/ConsumerAPIService/GetWaitingManualTasksByIdentityTests.cs
@@ -0,0 +1,50 @@
+namespace ProcessEngine.ConsumerAPI.Client.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System;
+    using System.Threading;
+
+    using EssentialProjects.IAM.Contracts;
+
+    using ProcessEngine.ConsumerAPI.Client.Tests.xUnit;
+    using ProcessEngine.ConsumerAPI.Contracts.DataModel;
+    using ProcessEngine.ConsumerAPI.Contracts;
+
+    using Xunit;
+
+    [Collection("ConsumerAPI collection")]
+    public class GetWaitingManualTasksByIdentityTests : ProcessEngineBaseTest
+    {
+        private readonly ConsumerAPIFixture fixture;
+
+        public GetWaitingManualTasksByIdentityTests(ConsumerAPIFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task BPMN_GetWaitingManualTasksByIdentity_ShouldFetchManualTaskList()
+        {
+            string processModelId = "test_consumer_api_manualtask";
+            var identity = DummyIdentity.Create();
+
+            ProcessStartResponsePayload processInstance = await this.fixture.ConsumerAPIClient.StartProcessInstance(
+                identity,
+                processModelId,
+                "StartEvent_1",
+                new ProcessStartRequestPayload<object>(),
+                StartCallbackType.CallbackOnProcessInstanceCreated);
+
+            // Give the process engine time to reach the user task
+            await Task.Delay(1000);
+
+            ManualTaskList manualTasks = await this.fixture.ConsumerAPIClient.GetWaitingManualTasksByIdentity(identity);
+
+            Assert.NotEmpty(manualTasks.ManualTasks);
+        }
+
+    }
+}
